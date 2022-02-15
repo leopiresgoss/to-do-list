9d65e62b636637893f3992de626b59924b0fefbc
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-02-15 11:35:48 -0600

    Install linters

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Output Management',
-      template: './src/index.html'
+      template: './src/index.html',
     }),
   ],
   devServer: {
@@ -16,7 +16,7 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true
+    clean: true,
   },
   module: {
     rules: [
