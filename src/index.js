940ce34907e664a9f5965b234c940fef11a03a4b
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-02-15 16:30:29 -0600

    Add scripts to display all tasks
    I also removed the list from index.html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 import './style.css';
+import {tasks, createList} from './modules/render-list/render-list.js' 
+
+// display all tasks
+createList(tasks);
---
+++ src/modules/render-list/render-list.js
@@ -0,0 +1,66 @@
+const tasks = [
+  {
+    index: 0,
+    description: "wash the dishes",
+    completed: false,
+  },
+  {
+    index: 1,
+    description: "complete To Do list project",
+    completed: false,
+  },
+  {
+    index: 2,
+    description: "complete To Do list project",
+    completed: false,
+  },
+]
+
+// generate elements
+const elementGenerator = (type, classNames) => {
+  const elem = document.createElement(type);
+  if(classNames) {
+    const classNamesArr = classNames.split(' ');
+    classNamesArr.forEach((className) => {
+      elem.classList.add(className)
+    });
+  }
+  return elem
+}
+
+const createTask = (task) => {
+  const idContent = `task-${task.index}`
+  const li = elementGenerator('li');
+  
+  const checkBtn = elementGenerator('button', 'check');
+  checkBtn.type = "button"
+  checkBtn.title = 'check';
+  
+  const label = elementGenerator('label', 'active');
+  label.textContent = task.description;
+  label.setAttribute('for', idContent);
+
+  const input = elementGenerator('input', 'task');
+  input.type = "text";
+  input.value = task.description;
+  input.setAttribute('maxlength', '255');
+  input.id = idContent;
+  
+  // update adding the image
+  const changeBtn = elementGenerator('button', 'change-order');
+
+  li.append(checkBtn, label, input, changeBtn);
+
+  return li
+}
+
+const createList = (tasks) => {
+  const list = document.querySelector('.list');
+
+  tasks.forEach((task) => {
+    const li = createTask(task);
+    list.appendChild(li);
+  })
+} 
+
+export {tasks, createList}
