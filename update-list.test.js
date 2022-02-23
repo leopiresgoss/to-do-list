5ac14475ee724f11f85daa28e0b10b0f0335a591
fbclh <fbclh@protonmail.com>
2022-02-23 12:59:51 -0300

    Set Jest boilerplate for the test

---
+++ update-list.test.js
@@ -0,0 +1,13 @@
+import UpdateList from './src/modules/render-list/update-list.js';
+
+describe('Add/Remove functions test', () => {
+  // Add function test
+  test('Add function test', () => {
+    expect(UpdateList.addTask()).toBe();
+  });
+
+  // Remove function test
+  test('Remove function test', () => {
+    expect(UpdateList.removeTask()).toBe();
+  });
+});
