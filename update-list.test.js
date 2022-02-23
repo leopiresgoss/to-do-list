b0170495b149252d4aa06924494654a743a25ca3
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-02-23 10:14:10 -0600

    Add addTask test

--- update-list.test.js
+++ update-list.test.js
@@ -1,13 +1,30 @@
 import UpdateList from './src/modules/render-list/update-list.js';
 
 describe('Add/Remove functions test', () => {
+  const todoList = {
+    listName: "Today's To Do",
+    tasks: [
+      {
+        index: 1,
+        description: 'test0',
+        completed: false,
+      },
+    ],
+  };
+
+  const expected1 = [{
+    index: 2,
+    description: 'test1',
+    completed: false,
+  }];
+
   // Add function test
   test('Add function test', () => {
-    expect(UpdateList.addTask()).toBe();
+    expect(UpdateList.addTask(todoList.tasks, 'test1')).toEqual(expect.arrayContaining(expected1));
   });
 
   // Remove function test
-  test('Remove function test', () => {
-    expect(UpdateList.removeTask()).toBe();
-  });
+  // test('Remove function test', () => {
+  //   expect(UpdateList.removeTask()).toBe();
+  // });
 });
